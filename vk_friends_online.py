28133e80ae68b196ae953eb38ac43656677198fc
alexander <alexander.i.kamenev@gmail.com>
2017-05-02 20:11:05 +0300

    start making task

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,17 @@
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6013620  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    # return '*'
+    return input('Please enter vk login: ')
 
 
 def get_user_password():
-    pass
+    # return '*'
+    return input('Please enter vk password: ')
 
 
 def get_online_friends(login, password):
@@ -17,9 +19,12 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    res = api.friends.getOnline()
+    # res = api.friends.get()
+    return res
 
 
 def output_friends_to_console(friends_online):
